dda8bcf02d6be13bf0f5f7755fafe4713dd50161
a.podgornova <a.podgornova@robo.finance>
2021-10-26 22:09:02 +0700

    curr state

--- src/test/java/task2/AutomationPracticeFormTest.java
+++ src/test/java/task2/AutomationPracticeFormTest.java
@@ -1,6 +1,38 @@
 package task2;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static com.codeborne.selenide.Selenide.*;
+
 public class AutomationPracticeFormTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
+
+    @Test
+    void testDemoForm() {
+
+        open("https://demoqa.com/automation-practice-form");
+        $("#firstName").setValue("Somefirstname");
+        $("#lastName").setValue("Somelastname");
+        $("#userEmail").setValue("some@email.com");
+        $x("//*[contains(text(), 'Female')]").click();
+        $("#userNumber").setValue("7123456789");
+
+        //datepicker
+        //$("#dateOfBirthInput").setValue("");
+        //$("#dateOfBirthInput").setValue("29 Sep 1980");
+
+
+
+
+    }
 
 
 }

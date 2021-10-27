e7d7ba8fb067f964b5f4bb80c871c6b0490bd305
a.podgornova <a.podgornova@robo.finance>
2021-10-27 21:57:05 +0700

    form filled

--- src/test/java/task2/AutomationPracticeFormTest.java
+++ src/test/java/task2/AutomationPracticeFormTest.java
@@ -4,10 +4,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selectors.by;
+import java.io.File;
+
 import static com.codeborne.selenide.Selenide.*;
 
 public class AutomationPracticeFormTest {
+
+    File picOk = new File("src/test/resources/interesting-cat-facts.jpg");
 
     @BeforeAll
     static void beforeAll() {
@@ -26,7 +29,19 @@ public class AutomationPracticeFormTest {
         $x("//*[contains(text(), 'Female')]").click();
         $("#userNumber").setValue("7123456789");
         Helpers.setDateById("dateOfBirthInput", "29 Sep 1980");
-        $("#subjectsInput").setValue("Math, Programming");
+        $("#subjectsInput").setValue("Maths").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
+        $x("//*[contains(text(), 'Music')]").click();
+        $("#uploadPicture").uploadFile(picOk);
+        $("#currentAddress").setValue("Some Area Some City Street st.1 fl.1");
+        $x("//*[contains(text(), 'Select State')]").click();
+        $("#react-select-3-option-1").click();
+        $x("//*[contains(text(), 'Select City')]").click();
+        $("#react-select-4-option-1").click();
+        $(".btn-primary").click();
+
+        //verify filled values
+
 
 
     }

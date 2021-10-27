c0228e8330937507497f1549acf67f05feeac2c3
a.podgornova <a.podgornova@robo.finance>
2021-10-27 17:30:45 +0700

    fill subjects

--- src/test/java/task2/AutomationPracticeFormTest.java
+++ src/test/java/task2/AutomationPracticeFormTest.java
@@ -3,12 +3,9 @@ package task2;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.JavascriptExecutor;
 
-import java.time.LocalDate;
-
+import static com.codeborne.selenide.Selectors.by;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public class AutomationPracticeFormTest {
 
@@ -21,15 +18,15 @@ public class AutomationPracticeFormTest {
     void testDemoForm() {
 
         open("https://demoqa.com/automation-practice-form");
+
+        //fill fields
         $("#firstName").setValue("Somefirstname");
         $("#lastName").setValue("Somelastname");
         $("#userEmail").setValue("some@email.com");
         $x("//*[contains(text(), 'Female')]").click();
         $("#userNumber").setValue("7123456789");
         Helpers.setDateById("dateOfBirthInput", "29 Sep 1980");
-
-
-
+        $("#subjectsInput").setValue("Math, Programming");
 
 
     }

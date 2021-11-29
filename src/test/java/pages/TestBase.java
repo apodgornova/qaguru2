5422c7b8d6404149a0dc0b4e21309ae45a05ea2c
a.podgornova <a.podgornova@robo.finance>
2021-11-29 22:13:17 +0700

    url

--- src/test/java/pages/TestBase.java
+++ src/test/java/pages/TestBase.java
@@ -23,6 +23,8 @@ public class TestBase {
             SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
             Configuration.startMaximized = true;
             //https://user1:1234@selenoid.autotests.cloud/wd/hub/
+
+            String selenoidUrl = System.getProperty("url", "selenoid.autotests.cloud/wd/hub/");
             Configuration.remote = format("https://%s:%s@%s",
                 credentialsConfig.login(),
                 credentialsConfig.password(),
